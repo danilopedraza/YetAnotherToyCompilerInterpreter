d0b432e8dfeb55588abf444651d66c04874dde06
César Pedraza <cpedraza@unal.edu.co>
2022-01-27 09:30:23 -0500

    todo list

--- myparser.py
+++ myparser.py
@@ -1,15 +1,33 @@
 from mylexer import Lexer
 
 class Parser:
-    def __init__(self, rules):
-        self.lexer = Lexer(rules["tokens"])
+    def __init__(self, lang):
+        self.lexer = Lexer(lang["tokens"])
 
         self.currentToken = None
         self.lookAhead = None
+
+        self.terminals    = {key[0] for key in lang["tokens"]}
+        self.nonTerminals = {key[0] for key in lang["grammar"]}
+
+        self.first = {}
+        self.follow = {}
+        self.computeFirst()
+        self.computeFollow()
+
+        self.table = {} 
+        self.computeTable()
+        
     
     def advanceTokens(self):
         self.currentToken = self.lookAhead
         self.lookAhead = self.lexer.nextToken()
     
     def parse(self, expr):
+        pass # TODO
+
+    def computeFirst(self):
+        pass # TODO
+
+    def computeFollow(self):
         pass # TODO
